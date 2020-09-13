520cebfed87a232c0d66b174e191b085f96757ae
Corbin133 <curtiscorbin08@gmail.com>
2020-09-13 12:46:13 -0400

    update for my_file_demo.py

--- my_file.py
+++ my_file.py
@@ -1,12 +1,2 @@
 print('hi git')
 print("I like pickles and I can shoot three's")
-
-
-print('another code change')
-
-def hi_there():
-    print("hi")
-
-def hey_there():
-
-    print("my local change")
